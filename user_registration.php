35307eac145e4d10aaad7786e72183cfb7fec220
mukuldevd <84217663+mukuldevd@users.noreply.github.com>
2024-08-10 21:36:30 +0530

    Update user_registration.php

--- user_registration.php
+++ user_registration.php
@@ -57,7 +57,7 @@ if (isset($_POST['submit'])){
 ?>
 <html lang="en">
 
-<!-- Mirrored from seantheme.com/studio/page_register.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 08 Aug 2024 06:31:55 GMT -->
+
 <head>
 <meta charset="utf-8">
 <title>Studio | Register</title>
@@ -164,5 +164,5 @@ Adjust the appearance to reduce glare and give your eyes a break.
 <script src="../cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="eb9de743829762ebff80794d-|49" defer></script><script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"8afd749378454dd9","version":"2024.7.0","r":1,"serverTiming":{"name":{"cfL4":true}},"token":"4db8c6ef997743fda032d4f73cfeff63","b":1}' crossorigin="anonymous"></script>
 </body>
 
-<!-- Mirrored from seantheme.com/studio/page_register.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 08 Aug 2024 06:31:55 GMT -->
+
 </html>
