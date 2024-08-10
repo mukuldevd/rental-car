d0fe1b25da0e385570a5f47fc2d031cccc8d05c5
mukuldevd <84217663+mukuldevd@users.noreply.github.com>
2024-08-10 21:35:56 +0530

    Update page_products.php

--- page_products.php
+++ page_products.php
@@ -17,7 +17,7 @@ $sql = "SELECT * FROM carprice";
 $result = $conn->query($sql);
 ?>
 
-<!-- Mirrored from seantheme.com/studio/page_products.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 08 Aug 2024 06:31:37 GMT -->
+
 <head>
 <meta charset="utf-8">
 <title>Studio | Products</title>
@@ -371,5 +371,5 @@ Adjust the appearance to reduce glare and give your eyes a break.
 <script src="../cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="59345fa577d7dffb08a9b947-|49" defer></script><script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"8afd7483ac144dd9","version":"2024.7.0","r":1,"serverTiming":{"name":{"cfL4":true}},"token":"4db8c6ef997743fda032d4f73cfeff63","b":1}' crossorigin="anonymous"></script>
 </body>
 
-<!-- Mirrored from seantheme.com/studio/page_products.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 08 Aug 2024 06:31:41 GMT -->
+
 </html>
